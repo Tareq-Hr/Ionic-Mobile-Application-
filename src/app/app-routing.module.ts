5a19e72526d85d57a54b2522922c3ac63e97c818
Tareq <tarikelhariri98@gmail.com>
2020-08-20 01:45:23 +0100

    First Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,11 +6,13 @@ const routes: Routes = [
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
+
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  
 ];
 
 @NgModule({
