55bb2604750e466fd760ddaf53529665e9ae123a
elbouni fati <fatimazahra.elbouni@usmba.ac.ma>
2020-08-30 21:01:45 +0100

    liste-places-logique

--- src/app/place-detaille/place-detaille.page.ts
+++ src/app/place-detaille/place-detaille.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AngularFireStorage} from '@angular/fire/storage';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-place-detaille',
   templateUrl: './place-detaille.page.html',
@@ -7,8 +9,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlaceDetaillePage implements OnInit {
 
-  constructor() { }
+  images = [];
+  constructor(
+    public afSG: AngularFireStorage,
+    public afDB: AngularFireDatabase,
+    public route:ActivatedRoute
+    
+  ) {
+    this.getImagesDatabase();
+  }
 
+
+
+getImagesDatabase() {
+  this.afDB.list('Places/').snapshotChanges(['child_added']).subscribe(images => {
+    images.forEach(image => {this.getImagesStorage(image);
+    });
+  });
+}
+
+getImagesStorage(image: any) {
+  const imgRef = image.payload.exportVal().images;
+  this.afSG.ref(imgRef).getDownloadURL().subscribe(imgUrl => {
+    console.log(imgUrl);
+    let param=this.route.snapshot.paramMap.get('id');
+     if(image.payload.exportVal().titre == param){
+    this.images.push({
+      name: image.payload.exportVal().titre,
+      url: imgUrl,
+      adresse: image.payload.exportVal().adresse,
+      long: image.payload.exportVal().longitude,
+      lat: image.payload.exportVal().latitude,
+      ville: image.payload.exportVal().ville,
+      pays: image.payload.exportVal().pays,
+      desc: image.payload.exportVal().mots_cles
+    });
+  }
+  });
+}
   ngOnInit() {
   }
 
