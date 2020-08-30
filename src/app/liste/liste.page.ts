850e479b8c60a62862ca2f120231a80da5737f44
Tareq <tarikelhariri98@gmail.com>
2020-08-30 21:35:47 +0100

    Adding The add-page-local logic

--- src/app/liste/liste.page.ts
+++ src/app/liste/liste.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AngularFireStorage} from '@angular/fire/storage';
+import { AngularFireDatabase} from '@angular/fire/database';
+
 @Component({
   selector: 'app-liste',
   templateUrl: './liste.page.html',
@@ -7,8 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListePage implements OnInit {
 
-  constructor() { }
+  images = [];
+  constructor(
+    public afSG: AngularFireStorage,
+    public afDB: AngularFireDatabase
+    
+  ) {
+    this.getImagesDatabase();
+  }
 
+
+
+getImagesDatabase() {
+  this.afDB.list('PlacesLocals/').snapshotChanges(['child_added']).subscribe(images => {
+    images.forEach(image => {
+      this.getImagesStorage(image);
+    });
+  });
+}
+
+getImagesStorage(image: any) {
+  const imgRef = image.payload.exportVal().images;
+  this.afSG.ref(imgRef).getDownloadURL().subscribe(imgUrl => {
+    console.log(imgUrl);
+    this.images.push({
+      name: image.payload.exportVal().titre,
+      url: imgUrl,
+    });
+  });
+}
   ngOnInit() {
   }
 
