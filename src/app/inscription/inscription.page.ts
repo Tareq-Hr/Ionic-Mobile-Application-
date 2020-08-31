7719bb682b0613042be8c2b8918572d6d902cf2e
sim-fpt-usmba <elaboui.hafida@gmail.com>
2020-08-31 02:10:03 +0200

    Auth

--- src/app/inscription/inscription.page.ts
+++ src/app/inscription/inscription.page.ts
@@ -1,45 +1,60 @@
-import { Component } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { User } from '../shared/user.class';
 
 @Component({
   selector: 'app-inscription',
   templateUrl: './inscription.page.html',
   styleUrls: ['./inscription.page.scss'],
 })
-export class InscriptionPage  {
-
+export class InscriptionPage implements OnInit {
+  user: User = new User();
   showPassword = false;
-passwordToggleIcon='eye';
+  passwordToggleIcon='eye';
+  showPassword1 = false;
+  passwordToggleIcon1='eye';
 
-showPassword1 = false;
-passwordToggleIcon1='eye';
+  constructor(private authSvc: AuthService, private router: Router) {}
 
-constructor() { 
-}
-togglePassword(): void {
-  this.showPassword = this.showPassword;
+  ngOnInit() {}
 
-  if(this.passwordToggleIcon == 'eye'){
-      this.showPassword = true;
-      this.passwordToggleIcon = 'eye-off';
-  }else{
-      this.showPassword = false;
-      this.passwordToggleIcon = 'eye';
+
+
+  togglePassword(): void {
+    this.showPassword = this.showPassword;
+
+    if(this.passwordToggleIcon == 'eye'){
+        this.showPassword = true;
+        this.passwordToggleIcon = 'eye-off';
+    }else{
+        this.showPassword = false;
+        this.passwordToggleIcon = 'eye';
+    }
+
+  }
+  togglePassword1(): void {
+    this.showPassword1 = this.showPassword1;
+
+    if(this.passwordToggleIcon1 == 'eye'){
+        this.showPassword1 = true;
+        this.passwordToggleIcon1 = 'eye-off';
+    }else{
+        this.showPassword1 = false;
+        this.passwordToggleIcon1 = 'eye';
+    }
+    
   }
 
-}
-togglePassword1(): void {
-  this.showPassword1 = this.showPassword1;
-
-  if(this.passwordToggleIcon1 == 'eye'){
-      this.showPassword1 = true;
-      this.passwordToggleIcon1 = 'eye-off';
-  }else{
-      this.showPassword1 = false;
-      this.passwordToggleIcon1 = 'eye';
+  async onRegister(){
+    const user = await this.authSvc.onRegister(this.user);
+    
+    if(user) {
+      console.log('successuflly');
+      this.router.navigateByUrl('/');
+    }
+    
   }
-  
-}
-
 
   
 
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,28 +1,45 @@
-import { Component} from '@angular/core';
+import { Component , OnInit} from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { User } from '../shared/user.class';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage{
-showPassword = false;
-passwordToggleIcon='eye';
+
+export class LoginPage implements OnInit{
+  user: User = new User();
+
+  showPassword = false;
+  passwordToggleIcon='eye';
 
 
-constructor() { 
-}
-togglePassword(): void {
-  this.showPassword = this.showPassword;
+  constructor(private router: Router, private authSvc: AuthService) {}
 
-  if(this.passwordToggleIcon == 'eye'){
-      this.showPassword = true;
-      this.passwordToggleIcon = 'eye-off';
-  }else{
-      this.showPassword = false;
-      this.passwordToggleIcon = 'eye';
+  ngOnInit() {}
+
+  togglePassword(): void {
+    this.showPassword = this.showPassword;
+
+    if(this.passwordToggleIcon == 'eye'){
+        this.showPassword = true;
+        this.passwordToggleIcon = 'eye-off';
+    }else{
+        this.showPassword = false;
+        this.passwordToggleIcon = 'eye';
+    }
   }
 
-}
+  async onLogin() {
+    const user = await this.authSvc.onLogin(this.user);
+    if (user) {
+      console.log('successuflly login');
+      this.router.navigateByUrl('/');
+    }
+
+
+  }
 }
 
